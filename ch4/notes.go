2d95d06f2817c2ac6442d074ee6452ee38d34c0d
David Hans <dahc36@gmail.com>
2024-01-08 19:40:35 -0300

    Add chapter 4

--- ch4/notes.go
+++ ch4/notes.go
@@ -57,7 +57,7 @@ func main() {
 
 	fmt.Println("-- For --")
 	// Go has 4 different formats of for
-	// 1. C-style for
+	// 1. C-style for (a.k.a. complete for loop)
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
@@ -113,12 +113,14 @@ func main() {
 	for k, v := range m {
 		fmt.Println(k, v)
 	}
+
+	fmt.Println("-- For - string --")
 	greet := "Good morning 🌞!"
 	for i, c := range greet {
 		fmt.Println(i, c, string(c))
 	}
 	// Indexes 14, 15 and 16 will be skipped over as 🌞 takes up 4 bytes to write.
-	// This is because for-range iterates over the runes, not the bytes.
+	// This is because for-range iterates over the string's runes, not the bytes.
 	// Multibyte runes are converted to a single 32-bit number (from their UTF-8 representation)
 	// and the offset is incremented by the number of bytes in the rune.
 	// If the loop encounters a byte that doesn't represent a valid UTF-8 value,
@@ -127,8 +129,86 @@ func main() {
 	// for-range values are copies
 	evenVals = []int{2, 4, 6, 8, 10, 12}
 	for _, v := range evenVals {
-		v = v * 2
+		v = v * 2 // This won't affect the values in evenVals
 	}
 	fmt.Println(evenVals)
-	// This won't affect the values in evenVals
+
+	fmt.Println("-- For - labeling --")
+	// You can label for statements to target them with continue or break statements
+	samples := []string{"hello", "apple_n!"}
+outer:
+	for _, sample := range samples {
+		for i, r := range sample {
+			fmt.Println(i, r, string(r))
+			if r == 'l' {
+				continue outer
+			}
+		}
+		fmt.Println()
+	}
+
+	// When to use each for loop
+	// - for-range is the best way of iterating through the entire content of a compound type
+	// - The c-style loop is useful when you only want to iterate a sub-section
+	//   - Be careful that this won't work when iterating strings! (because of the bytes vs runes)
+	// - The conditional for replaces the classic "while" statement
+	// - The infinite for can replicate the "do-while" pattern and implement the iterator pattern
+
+	fmt.Println("-- Switch --")
+	// Go has better switch statements than your average language
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			fmt.Println(word, "is exactly the right length:", size)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word!")
+		}
+	}
+	// - There's no fallthrough by default on every case
+	//   - If you want fallthrough you can use the fallthrough keyword but try to avoid it refactoring
+	// - Cases with empty bodies just don't do anything
+	// - break statements are not necessary but you can use them
+	//   - Try to avoid and refactor
+	//   - You may want to break out of an outer loop from inside a case, you'll need to label that loop
+
+	fmt.Println("-- Switch - Blank Switches --")
+	for _, word := range words {
+		switch size := len(word); {
+		case size < 5:
+			fmt.Println(word, "is a short word!")
+		case size == 5:
+			fmt.Println(word, "is exactly the right length:", size)
+		case size > 9:
+			fmt.Println(word, "is a long word!")
+		default:
+		}
+	}
+	// Don't overdo it with this power, if you write a blank switch with only equality comparisons,
+	// you should replace it with an expression (regular) switch statement
+
+	// Use switch statements to group related cases
+	// If you want to do unrelated comparisons use if/else
+
+	fmt.Println("-- goto --")
+	// Go's goto has some rules:
+	// 1. You can't jump over declarations
+	// 2. You can't jump into an inner or parallel block
+
+	a := rand.Intn(10)
+	for a < 100 {
+		if a%5 == 0 {
+			goto done
+		}
+		a = a*2 + 1
+	}
+	fmt.Println("do something when the loop completes normally")
+done:
+	fmt.Println("do complicated stuff no matter why we left the loop")
+	fmt.Println(a)
+	// Try to avoid using goto, but if it is the best option to make the code readable
+	// it's theres
 }
